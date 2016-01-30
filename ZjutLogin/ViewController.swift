9babfa6d49b9e3f94d931d35349e0b9b57f489ef
kojirou <kojirouhtc@gmail.com>
2016-01-31 01:37:39 +0800

    Initial Commit

---
+++ ZjutLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZjutLogin
+//
+//  Created by 王宇 on 16/1/31.
+//  Copyright © 2016年 Kojirou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
